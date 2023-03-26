09e972274ca6eb5efc9469c34b90a37d2312c9c1
Fredrik Ridderfalk <ridderfalk@gmail.com>
2023-03-26 20:44:52 +0200

    feat: add footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,25 @@
+import logo from "../assets/logo.png";
 import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <div>Left-side container</div>
-      <div>Right-side container</div>
+      <div className="container__footer-content">
+        <div>
+          <img src={logo} alt="Esplanad logo." className="footer-logo" />
+        </div>
+        <div className="container__footer-links">
+          <a href="" className="footer-link">
+            GDPR
+          </a>
+          <a href="" className="footer-link">
+            Cookies
+          </a>
+          <a href="" className="footer-link">
+            Jobba hos oss
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
