64856338eaaeda16ee4d72dbbcee7c1dce5aa4c4
Fredrik Ridderfalk <ridderfalk@gmail.com>
2023-04-01 11:09:33 +0200

    feat: add responsiveness to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,12 @@ function Footer() {
   return (
     <div className="footer">
       <div className="container__footer-content">
-        <div>
+        <div className="container__logo-address">
           <img src={logo} alt="Esplanad logo." className="footer-logo" />
+          <div className="container__address">
+            <p>Datavägen 12B</p>
+            <p>436 32 Askim</p>
+          </div>
         </div>
         <div className="container__footer-links">
           <a href="" className="footer-link">
