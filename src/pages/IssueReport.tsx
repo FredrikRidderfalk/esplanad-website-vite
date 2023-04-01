50a7e54b32d9f50e27a2f0db0d70256a733b57c1
Fredrik Ridderfalk <ridderfalk@gmail.com>
2023-04-01 15:20:15 +0200

    feat: add content for Offer & IssueReport

--- src/pages/IssueReport.tsx
+++ src/pages/IssueReport.tsx
@@ -3,7 +3,32 @@ import "./IssueReport.css";
 function IssueReport() {
   return (
     <div className="issue-report">
-      <h1>This is the IssueReport page</h1>
+      <div className="container__issue-report-content">
+        <div className="issue-report-content">
+          <h2>Felanmälan</h2>
+          <p>
+            Om ni har har en felanmälan under dagtid (07:00 – 16:00) skall ni
+            ringa till 031-788 28 00
+          </p>
+          <p>
+            För infomation om hur vi hanterar era personuppgifter, vänligen se
+            fliken GDPR här på hemsidan
+          </p>
+          <p>
+            Vid akuta ärenden under övrig tid (17:00-07:00) skall dessa göras
+            till den jourtjänst din förening har avtal med.
+          </p>
+          <p>
+            Telefonnumret till jourtjänsten i din fastighet står angivet på
+            anslagstavlan i entrén.
+          </p>
+          <p>
+            Är er felanmälan inte akut går det bra att göra denna genom att ni
+            fyller i webbformuläret: (Obs! det är viktigt att ni fyller i rätt
+            uppgifter i felanmälan, annars kan vi inte avhjälpa er felanmälan)
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Offer.tsx
+++ src/pages/Offer.tsx
@@ -3,7 +3,20 @@ import "./Offer.css";
 function Offer() {
   return (
     <div className="offer">
-      <h1>This is the Offer page</h1>
+      <div className="container__offer-content">
+        <div className="offer-content">
+          <h2>Offert</h2>
+          <p>
+            Om ni önskar en offert på våra tjänster är ni välkomna att kontakta
+            oss genom att ringa 031-7882800, skicka ett mail till{" "}
+            <a className="email-link" href="roger@esplanad.nu">
+              info@esplanad.nu
+            </a>{" "}
+            eller använda formuläret nedan så återkommer vi med ett svar inom
+            kort.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
